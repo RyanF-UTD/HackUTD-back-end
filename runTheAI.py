4851ffcb254ca8b61121dcbf4fcc2f7740fbdc05
TusharW4ni <reachtusharwani@gmail.com>
2023-11-05 09:09:58 -0600

    finished the AI?

--- runTheAI.py
+++ runTheAI.py
@@ -1,29 +1,14 @@
-# from huggingface_hub import notebook_login
-
-# # Use a pipeline as a high-level helper
-# from transformers import pipeline
-
-# pipe = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-vqa")
-
-
-# from PIL import Image
-
-# image_path = "./hotel-room-in-bad-condition.jpg"
-# image = Image.open(image_path)
-# text = "What are the defects in the image? Defects are basically things that seem wrong with all the features in the room."
-# text2 = "Describe the image to me."
-
-
-# out = pipe(image, text)
-
-# print(out)
-# print(out2)
-
-
 # Load model directly
 from transformers import ViltProcessor, ViltForQuestionAnswering
 
-def answer_question(question):
+def answer_question(question, image_path):
+    processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+    model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+
+    from PIL import Image
+
+    #image_path = "./resiInt/resiInt1.jpeg"
+    image = Image.open(image_path)
     encoding = processor(image, question, return_tensors="pt")
     outputs = model(**encoding)
     sorted_indexs = outputs.logits.argsort(dim=-1, descending=True)[0].tolist()
@@ -32,88 +17,112 @@ def answer_question(question):
 
     return top_prediction
 
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+#loop over all images in the /resiInt folder and ask the AI questions about them
+# get resiInt folder size
+import os
+path, dirs, files = next(os.walk("./resiInt"))
+file_count = len(files)
+data_set = {}
+for i in range(file_count):
+    if (i != file_count-1):
+        image_name = "./resiInt/resiInt" + str(i+1) + ".jpeg"
 
-from PIL import Image
+        text = "Is this image of the inside or outside of a building?"
+        if (answer_question(text, image_name) == "inside"):
 
-image_path = "./hotel-room-in-bad-condition.jpg"
-image = Image.open(image_path)
+            text = "Is the floor made of valuable materials?"
+            if (answer_question(text, image_name) == "yes"):
+                
+                text = "Is the floor made of wood?"
+                if (answer_question(text, image_name) == "yes"):
+                    data_set["wood"] = "yes"
+                elif (answer_question(text, image_name) == "no"):
+                    data_set["wood"] = "no"
 
-text = "Is this image of the inside or outside of a building?"
-if (answer_question(text) == "inside"):
+                text = "Is the floor made of tiles?"
+                if (answer_question(text, image_name) == "yes"):
+                    data_set["tiles"] = "yes"
+                elif (answer_question(text, image_name) == "no"):
+                    data_set["tiles"] = "no"
 
-    text = "Is the floor made of valuable materials?"
-    if (answer_question(text) == "yes"):
-        
-        text = "Is the floor made of wood?"
-        if (answer_question(text) == "yes"):
-            print("The floor is made of wood.")
-        elif (answer_question(text) == "no"):
-            text = "Is the floor made of marble?"
-            if (answer_question(text) == "yes"):
-                print("The floor is made of marble.")
-            elif (answer_question(text) == "no"):
-                print("The floor is made of other valuable materials.")
-    elif (answer_question(text) == "no"):
-        print("This is the inside of a building with no valuable materials.")
+                text = "Is the floor made of marble?"
+                if (answer_question(text, image_name) == "yes"):
+                    data_set["marble"] = "yes"
+                elif (answer_question(text, image_name) == "no"):
+                    data_set["marble"] = "no"
 
-    text = "Do the walls have cracks on them?"
-    if (answer_question(text) == "yes"):
-        text = "Do the walls look like they are about to collapse?"
-        if (answer_question(text) == "yes"):
-            print("The walls are cracked and about to collapse.")
-        elif (answer_question(text) == "no"):
-            print("The walls are cracked but not about to collapse.")
-    elif (answer_question(text) == "no"):
-        print("The walls are not cracked.")
+            elif (answer_question(text, image_name) == "no"):
+                data_set["valuable materials"] = "no"
 
-    text = "Does the ceiling look like it has cracks on it?"
-    if (answer_question(text) == "yes"):
-        text = "Does the ceiling look like it is about to collapse?"
-        if (answer_question(text) == "yes"):
-            print("The ceiling is cracked and about to collapse.")
-        elif (answer_question(text) == "no"):
-            print("The ceiling is cracked but not about to collapse.")
-    
-    text = "Do the windows have cracks in them?"
-    if (answer_question(text) == "yes"):
-        print("The windows are cracked.")
-    elif (answer_question(text) == "no"):
-        print("The windows are not cracked.")
+            text = "Do the walls have cracks on them?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["wallCracks"] = "yes"
+                text = "Do the walls look like they are about to collapse?"
+                if (answer_question(text, image_name) == "yes"):
+                    data_set["wallCollapse"] = "yes"
+                elif (answer_question(text, image_name) == "no"):
+                    data_set["wallCollapse"] = "no"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["wallCracks"] = "no"
 
-    text = "Do the doors look like they have been damaged?"
-    if (answer_question(text) == "yes"):
-        print("The doors are damaged.")
-    elif (answer_question(text) == "no"):
-        print("The doors are not damaged.")
+            text = "Does the ceiling look like it has cracks in it?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["ceilingCracks"] = "yes"
+                text = "Does the ceiling look like it is about to collapse?"
+                if (answer_question(text, image_name) == "yes"):
+                    data_set["ceilingCollapse"] = "yes"
+                elif (answer_question(text, image_name) == "no"):
+                    data_set["ceilingCollapse"] = "no"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["ceilingCracks"] = "no"
+            
+            text = "Do the windows look like they have been damaged?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["windowsDamaged"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["windowsDamaged"] = "no"
 
-    
-elif (answer_question(text) == "outside"):
-    text = "Is the building made of valuable materials?"
-    if (answer_question(text) == "yes"):
-        text = "Is the building made of wood?"
-        if (answer_question(text) == "yes"):
-            print("The building is made of wood.")
-        elif (answer_question(text) == "no"):
+            text = "Do the doors look like they have been damaged?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["doorsDamaged"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["doorsDamanged"] = "no"
+            
+        elif (answer_question(text, image_name) == "outside"):
+            text = "Is the building made of wood?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["outsideMaterialWood"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["outsideMaterialWood"] = "no" 
+
+            text = "Is the building made of bricks?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["outsideMaterialBrick"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["outsideMaterialBrick"] = "no"
+
+            text = "Is the building made of glass?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["outsideMaterialGlass"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["outsideMaterialGlass"] = "no"
+
             text = "Is the building made of marble?"
-            if (answer_question(text) == "yes"):
-                print("The building is made of marble.")
-            elif (answer_question(text) == "no"):
-                print("The building is made of other valuable materials.")
+            if (answer_question(text, image_name) == "yes"):
+                data_set["outsideMaterialMarble"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["outsideMaterialMarble"] = "no"
 
-    text = "Do the walls have cracks on them?"
-    if (answer_question(text) == "yes"):
-        text = "Do the walls look like they are about to collapse?"
-        if (answer_question(text) == "yes"):
-            print("The walls are cracked and about to collapse.")
-        elif (answer_question(text) == "no"):
-            print("The walls are cracked but not about to collapse.")
-    elif (answer_question(text) == "no"):
-        print("The walls are not cracked.")
+            text = "Do you see any defects in the outside?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["doYouSeeDefects"] = "yes"
+                text = "What is that defect?"
+                data_set["whatIsThatDefect"] = answer_question(text, image_name)
+            elif (answer_question(text, image_name) == "no"):
+                data_set["doYouSeeDefects"] = "no"
 
-    text = "Would it be a safe neighborhood to live in?"
-    if (answer_question(text) == "yes"):
-        print("This is a safe neighborhood.")
-    elif (answer_question(text) == "no"):
-        print("This is not a safe neighborhood.")
+            text = "Would it be a safe neighborhood to live in?"
+            if (answer_question(text, image_name) == "yes"):
+                data_set["isItASafeNeighborhood"] = "yes"
+            elif (answer_question(text, image_name) == "no"):
+                data_set["isItASafeNeighborhood"] = "no"
