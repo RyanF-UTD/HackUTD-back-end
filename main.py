e993d3f3e4aba25ec833ec1fe9e1974b337f939b
Jarrod Rogers <jtr_gamer@hotmail.com>
2023-11-05 05:35:51 -0600

    Stubbed functions

--- main.py
+++ main.py
@@ -21,17 +21,17 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/buy")
-async def process_transaction(req: RequestBody):
+async def process_buy(req: RequestBody):
     return req
 
 @app.post("/sell")
-async def process_transaction(req: RequestBody):
+async def process_sell(req: RequestBody):
     return req
 
 @app.post("/register")
-async def process_transaction(req: str):
+async def process_register(req: str):
     return {"message": req}
 
 @app.post("/value")
-async def process_transaction(req: str):
+async def process_value(req: str):
     return {"message": req}
